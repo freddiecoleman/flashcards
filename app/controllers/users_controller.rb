5f91665e9cbe5423da2f2b85252009fa58175216
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-02-08 21:24:41 +0000

    Added auto login with temporary cookie after creating a new  user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.id
+			cookies[:auth_token] = @user.auth_token
 			redirect_to root_url, notice: "Thank you for signing up!"
 		else
 			render "new"
