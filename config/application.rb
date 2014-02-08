aeac7fe51db9a674d54104f94f172f88d9b88247
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-02-08 11:13:26 +0000

    Fixed errors due to depraciated code structure

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+I18n.enforce_available_locales = false
+
 module Flashcards
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
