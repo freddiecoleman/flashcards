a2c225dc81441f9ccfde57a4f0a080bbc7e7ba71
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-02-09 12:48:32 +0000

    Added JavaScript to review interface

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,4 +1,12 @@
 class ReviewController < ApplicationController
+	before_filter :authorise
 	def index
+		@flashcard = @current_user.flashcards.first if @flashcard == nil
+
 	end
+
+	def show
+		render partial: 'answer'
+	end
+	
 end
