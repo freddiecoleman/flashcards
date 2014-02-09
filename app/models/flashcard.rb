d97b206a0a7a1c8a5179b84cb7eba4d6b35f1573
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-02-09 14:21:41 +0000

    Added due date to flashcards

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -2,4 +2,22 @@ class Flashcard < ActiveRecord::Base
 	validates :front, presence: true
 	validates :back, presence: true
 	belongs_to :deck
+	before_create :default_values
+
+	def due_in
+		if self.due > Time.now
+			"in " + Helpers.distance_of_time_in_words(Time.now, self.due, include_seconds: true)
+		else
+			"Due"
+		end
+	end
+
+private
+
+    def default_values
+      self.due ||= Time.now + 2.weeks
+    end
+    module Helpers
+		extend ActionView::Helpers::DateHelper
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208214914) do
+ActiveRecord::Schema.define(version: 20140209133421) do
 
   create_table "decks", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140208214914) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "deck_id"
+    t.datetime "due"
   end
 
   add_index "flashcards", ["deck_id"], name: "index_flashcards_on_deck_id"
