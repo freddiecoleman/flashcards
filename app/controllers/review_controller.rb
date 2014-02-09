59b78ef8b812d2b29d6e376b4011e2d040edc67b
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-02-09 11:22:33 +0000

    Implemented initial review interface

---
+++ app/controllers/review_controller.rb
@@ -0,0 +1,4 @@
+class ReviewController < ApplicationController
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Flashcards::Application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  resources :review
   resources :users
   resources :sessions
   resources :password_resets
