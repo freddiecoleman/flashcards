a83b92c0e6a14ad1d53d5fe19f36d811d2e30479
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-02-13 17:40:24 +0000

    Tidied up home page spec with some variables

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,32 +1,42 @@
 require 'spec_helper'
 
 describe "welcome" do
+
+  register_link = 'Register'
+  login_link = 'Login'
+  stats_link = 'Stats'
+  manage_decks_link = 'Manage Decks'
+  manage_flashcards_link = 'Manage Flashcards'
+
   subject { page }
   describe "home page" do
     before { visit root_path }
 
     describe "not logged in" do
       it "should display logged out navbar" do
-        page.should have_link 'Register'
-        page.should have_link 'Login'
+        page.should have_link register_link
+        page.should have_link login_link
       end
       it "should not display logged in user navbar" do
-        page.should have_no_link 'Stats'
-        page.should have_no_link 'Manage Decks'
-        page.should have_no_link 'Manage Flashcards'
+        page.should have_no_link stats_link
+        page.should have_no_link manage_decks_link
+        page.should have_no_link manage_flashcards_link
       end
     end
 
     describe "logged in" do
-      before { register }
+      before do 
+        register
+        visit root_path
+      end
       it "should display logged in user navbar" do
-        page.should have_link 'Stats'
-        page.should have_link 'Manage Decks'
-        page.should have_link 'Manage Flashcards'
+        page.should have_link stats_link
+        page.should have_link manage_decks_link
+        page.should have_link manage_flashcards_link
       end
       it "should not display logged out navbar" do
-        page.should have_no_link 'Register'
-        page.should have_no_link 'Login'
+        page.should have_no_link register_link
+        page.should have_no_link login_link
       end
     end
   end
