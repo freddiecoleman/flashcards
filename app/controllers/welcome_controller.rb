9b1fc1c9bfd95e162e7e23b49c65575b61518b1f
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-04-22 16:28:06 +0100

    Adding logged in user info and decks to front page when logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  before_filter :current_user
   def index
+
+    @deck = Deck.new if current_user
+    @decks = @current_user.decks.to_a if current_user
   end
 end
