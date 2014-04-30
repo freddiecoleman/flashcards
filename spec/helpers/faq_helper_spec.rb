67b3c9fc4dfbbae2487630517124204081315983
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-04-30 18:47:43 +0100

    Added FAQ, Tutorial, and Contact Us

---
+++ spec/helpers/faq_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the FaqHelper. For example:
+#
+# describe FaqHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+describe FaqHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
